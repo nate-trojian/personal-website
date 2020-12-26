bcd66f75c1171c90f04ccaa6ac56a76d7cc7a602
Nathaniel Trojian <ntrojian@gmail.com>
2020-12-25 21:42:19 -0500

    Add animation to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import Fade from 'react-reveal/Fade'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faClipboardCheck, faDrawPolygon} from '@fortawesome/free-solid-svg-icons'
 import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
@@ -7,20 +8,22 @@ import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
 const SocialFooter = () => {
     return (
         <footer className="footer">
-            <div className="social-links">
-                <Link to="#">
-                    <FontAwesomeIcon className="social" icon={faDrawPolygon} size="3x" />
-                </Link>
-                <Link to="#">
-                    <FontAwesomeIcon className="social" icon={faClipboardCheck} size="3x" />
-                </Link>
-                <a href="https://www.linkedin.com/in/nathaniel-trojian-269010104/" rel="noopener noreferrer" target="_blank">
-                    <FontAwesomeIcon className="social" icon={faLinkedin} size="3x" />
-                </a>
-                <a href="https://github.com/nate-trojian" rel="noopener noreferrer" target="_blank">
-                    <FontAwesomeIcon className="social" icon={faGithub} size="3x" />
-                </a>
-            </div>
+            <Fade bottom={true} distance="25px" delay={3000}>
+                <div className="social-links">
+                    <Link to="/blog">
+                        <FontAwesomeIcon className="social" icon={faDrawPolygon} size="2x" />
+                    </Link>
+                    <Link to="/resume">
+                        <FontAwesomeIcon className="social" icon={faClipboardCheck} size="2x" />
+                    </Link>
+                    <a href="https://www.linkedin.com/in/nathaniel-trojian-269010104/" rel="noopener noreferrer" target="_blank">
+                        <FontAwesomeIcon className="social" icon={faLinkedin} size="2x" />
+                    </a>
+                    <a href="https://github.com/nate-trojian" rel="noopener noreferrer" target="_blank">
+                        <FontAwesomeIcon className="social" icon={faGithub} size="2x" />
+                    </a>
+                </div>
+            </Fade>
         </footer>
     )
 }
