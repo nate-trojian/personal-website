b0d1d5c0d677d708ab2936a7dac8f60c71a91c4f
Nathaniel Trojian <ntrojian@gmail.com>
2021-01-03 15:15:06 -0500

    Make blog a component

---
+++ src/components/blog.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+        }
+      }
+    }
+  }
+`
+
+const Blog = () => {
+  const data = useStaticQuery(query)
+  const posts = data.allMarkdownRemark.nodes
+
+  if (posts.length === 0) {
+    return (
+      <div id="global-wrapper">
+        <Seo />
+        <p>
+          No blog posts found
+                </p>
+        <SocialFooter />
+      </div>
+    )
+  }
+
+  return (
+    <div id="global-wrapper">
+      <Seo />
+      <section id="blog-posts">
+        <Container>
+          {posts.map(post => (
+            <Row>
+              <Col>
+                <h3>{post.frontmatter.title}</h3>
+                <p dangerouslySetInnerHTML={{
+                  __html: post.frontmatter.description || post.excerpt,
+                }} />
+                <small className="text-muted">{post.frontmatter.date}</small>
+              </Col>
+              <Col>
+                <img alt="test"></img>
+              </Col>
+            </Row>
+          ))}
+        </Container>
+      </section>
+      <SocialFooter />
+    </div>
+  )
+}
+
+export default Blog
